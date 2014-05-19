fd9851dd6b610abc4b72b08836cee08aa9e5292e
Rhys Oxenham <roxenham@redhat.com>
2014-05-20 00:41:33 +0100

    s/https/http/g for repos

--- staypuft_poc.py
+++ staypuft_poc.py
@@ -138,7 +138,7 @@ def begin_install(fqdn):
 			   stdout=devnull, stderr=devnull)
 	except: die("STAYPUFT INSTALL")
 	print "\nINFO: Configuration complete. See files in /root/staypuft_files/"
-	print "INFO: Installation media can be found at: https://%s:8080/repos/rheldvd-el6" % fqdn
+	print "INFO: Installation media can be found at: http://%s:8080/repos/rheldvd-el6" % fqdn
 	print "INFO: Repositories are available at: http://%s:8080/repos" % fqdn
 	print "SUCCESS: Staypuft is ready to be configured, run 'staypuft-installer'"
 
